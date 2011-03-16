2b745c3888f29faf439d56ccb51ccab17a84de59
Foaly* <hi@jonathanschmid.de>
2011-03-16 14:55:32 +0100

    english language file

---
+++ lang/en_US.php
@@ -0,0 +1,26 @@
+<?php
+$lang['friendlyname'] = 'Availability';
+$lang['postinstall'] = 'Be sure to set "Use JSAvailability" permissions to use this module!';
+$lang['postuninstall'] = 'Modul unistalled.';
+$lang['really_uninstall'] = 'Really? You\'re sure you want to uninstall this module?';
+$lang['uninstalled'] = 'Module uninstalled.';
+$lang['installed'] = 'Module version %s installed.';
+$lang['accessdenied'] = 'Access Denied. Please check your permissions.';
+$lang['error'] = 'Error!';
+$lang['help'] = 'No help.';
+$lang['changelog'] = '';
+$lang['moddescription'] = 'Manage availability in an annual view (AJAX-based).';
+
+$lang['calendar-view'] = 'Calendar view';
+$lang['selectyear'] = 'Show year';
+$lang['wrongdateformat'] = 'Error: Wrong date format!';
+$lang['overlap'] = 'Not possible: Entries overlap';
+$lang['saved'] = 'Successfully saved.';
+
+$lang['preferences'] = 'Preferences';
+$lang['append_months_before'] = 'Append years at the beginning';
+$lang['append_months_after'] = 'Append years at the end';
+$lang['submit'] = 'Send';
+$lang['cancel'] = 'Cancel';
+$lang['preferencessaved'] = 'Preferences saved.';
+?>
